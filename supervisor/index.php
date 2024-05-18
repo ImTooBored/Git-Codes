9cc7c4b8baa54a13bdee56a66a3022be208391e8
pat0t0ya <yestinprado@gmail.com>
2024-05-18 12:27:51 +0800

    setting notification

---
+++ supervisor/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notification and Approvals</title>
+
+</head>
+<body>
+
+<div class="button-container">
+    <a href="notification.php" class="button">Notification</a> <br>
+    <a href="approvals.php" class="button">Approvals</a>
+</div>
+
+</body>
+</html>
--- supervisor/notification.php
+++ supervisor/notification.php
@@ -1,12 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notification and Approvals</title>
+
+</head>
+<body>
+
+<div class="button-container">
+    <a href="notification.php" class="button">Notification</a> <br>
+    <a href="approvals.php" class="button">Approvals</a>
+</div>
+
+</body>
+</html>
+
 <?php
     require_once 'DB connection.php';
-    $sql = "SELECT * FROM NOTIFICATION
+    $sql = "SELECT subject,content,date FROM NOTIFICATION
             WHERE type = 'supervisor'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "<br>".$row["notif_id"]."<br>".$row["subject"]."<br>".$row["type"]."<br>".$row["content"]."<br>".$row["attachment"]."<br>".$row["date"];
+            echo "<br>".$row["subject"].$row["date"]."<br>".$row["content"]."<br>";
         }
     } else {
         echo "0 results";
