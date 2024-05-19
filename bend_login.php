1f64a13fa54b2afd0d83a88043d0c2c12aec2c40
pat0t0ya <yestinprado@gmail.com>
2024-05-19 14:47:56 +0800

    updated ben login and login fornt end

--- bend_login.php
+++ bend_login.php
@@ -1,17 +1,10 @@
 <?php
     require_once "API.php";
-    require_once "DB connection";
-    function clean_input($data) {
-        $data = trim($data);
-        $data = htmlspecialchars($data);
-        $data = preg_replace('/\b(?:https?|ftp):\/\/\S+/', '', $data);
-        return $data;
-    }
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-        $email = clean_input($_POST("email"));
-        $password = clean_input($_PSOT("password"));
+    require_once "DB connection.php";
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
         echo $email,"<br>",$password;
-
+    }
 ?>
